c1f57ca4febe79e772e3701619937c4dd3d21913
byeolbit <info@byeolbit.com>
2017-07-24 03:23:55 +0700

    스케쥴,유의사항 템플릿 완성

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     let $ball = $('.mirror-ball-container');
     let $one = $('#one');
+    let $joinBtn = $('.join-button');
+    let $navBar = $('nav');
+
+    if ($(window).scrollTop() > $joinBtn.offset().top) {
+            $navBar.fadeIn();
+    }
 
     for (let i=0; i<100; i++) {
         $('<div class="mirror-ball-particle"></div>').appendTo($ball);
@@ -9,8 +15,24 @@ $(document).ready(function(){
     let $learnMore = $('.learn-more');
     $learnMore.click(function(){
          $('html, body').animate({
-            scrollTop: $one.offset().top
+            scrollTop: $one.offset().top-$navBar.height()
         }, 1000, 'swing');;
     });
+
+    let $navTitle = $('.nav-title');
+    $navTitle.click(function(){
+         $('html, body').animate({
+            scrollTop: 0
+        }, 1000, 'swing');;
+    });
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > $joinBtn.offset().top) {
+            $navBar.fadeIn();
+        } else {
+            $navBar.fadeOut();
+        }
+    });
+
 
 });
